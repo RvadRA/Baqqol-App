b2855ed1e153b3d99aca9bdfb894da7a494a78e4
Abdusomad <120725065+RvadRA@users.noreply.github.com>
2026-02-12 12:11:57 +0500

    Use environment variable for socket API URL

--- frontend/src/socket.ts
+++ frontend/src/socket.ts
@@ -1,7 +1,7 @@
 // socket.ts - ФИНАЛЬНАЯ УПРОЩЕННАЯ ВЕРСИЯ
 import { io, Socket } from "socket.io-client";
 // Create socket instance
-export const socket: Socket = io("http://localhost:5000", {
+export const socket: Socket = io(import.meta.env.VITE_API_URL), {
   autoConnect: false, // НЕ подключаться автоматически
   auth: (cb) => {
     const token = localStorage.getItem("token");
